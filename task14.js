4df4c13fef2550df02a4d09768968d5d6243c96f
biniyamz <binizda@gmail.com>
2014-12-12 16:59:47 +0100

    Create task14.js

---
+++ task14.js
@@ -0,0 +1,11 @@
+ 
+// Task 1 - THE AGE CALCULATOR
+function caculateAge( birthYear , currretYear ) {
+//add code here
+}
+ 
+// Task 2 - THE LIFETIME SUPPLY CALCULATOR
+ 
+function calculateSupply( age , amount ) {
+//add code here
+} 
